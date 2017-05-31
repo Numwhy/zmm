582376a1c19e5f2a727a0f7048cb31e071df156a
Numwhy <758866744@qq.com>
2017-05-31 12:14:11 +0800

    zmm
    真目明官网

---
+++ js/fahuo.js
@@ -0,0 +1,176 @@
+/**
+ *******************************************************************
+ ************* ─────────────────── **************
+ ************* ──  WFPHP在线订单系统官方正式版 ── **************
+ ************* ─────────────────── **************
+ ************* 官方网站：http://www.wf1805.cn/        **************
+ ************* 官方店铺：http://889889.taobao.com/    **************
+ ************* 程序开发：WENFEI                       **************
+ ************* 客户服务：[淘宝旺旺] yygynui613        **************
+ ************* 技术支持：[腾讯QQ] 183712356           **************
+ *******************************************************************
+ *************   官方正版 *** 版权所有 *** 盗版必究   **************
+ *******************************************************************
+ */
+
+function GetDateStr(AddDayCount){
+	var dd = new Date();
+	dd.setDate(dd.getDate()+AddDayCount);
+	var y = dd.getFullYear();
+	var m = dd.getMonth()+1;
+	var d = dd.getDate();
+	return y+"-"+m+"-"+d;
+}
+
+var i=0;
+for(i=0;i<22;i++){
+	document.write(GetDateStr(-1));	
+	var rand1=parseInt(Math.random()*32+1);	
+	quotes=new Array 
+quotes[1]=' 河北的' 
+quotes[2]=' 山西的' 
+quotes[3]=' 辽宁的' 
+quotes[4]=' 吉林的' 
+quotes[5]=' 黑龙江的' 
+quotes[6]=' 江苏的' 
+quotes[7]=' 浙江的' 
+quotes[8]=' 广西的' 
+quotes[9]=' 安徽的' 
+quotes[10]=' 福建的' 
+quotes[11]=' 江西的' 
+quotes[12]=' 山东的' 
+quotes[13]=' 河南的' 
+quotes[14]=' 内蒙古的' 
+quotes[15]=' 湖北的' 
+quotes[16]=' 重庆的' 
+quotes[17]=' 湖南的' 
+quotes[18]=' 广东的' 
+quotes[19]=' 海南的' 
+quotes[20]=' 深圳的' 
+quotes[21]=' 四川的' 
+quotes[22]=' 贵州的'
+quotes[23]=' 西藏的'
+quotes[24]=' 云南的'
+quotes[25]=' 陕西的'
+quotes[26]=' 宁夏的'
+quotes[27]=' 甘肃的'
+quotes[28]=' 青海的'
+quotes[29]=' 北京的'
+quotes[30]=' 天律的'
+quotes[31]=' 新疆的'
+quotes[32]=' 上海的'
+	var quote=quotes[rand1]
+	document.write(quote)	
+	var rand1=parseInt(Math.random()*55+1);	
+	quotes=new Array
+	quotes[1]='赵先生' 
+quotes[2]='钱女士' 
+quotes[3]='孙先生' 
+quotes[4]='李女士' 
+quotes[5]='周先生' 
+quotes[6]='吴女士' 
+quotes[7]='郑女士' 
+quotes[8]='王先生' 
+quotes[9]='冯女士' 
+quotes[10]='陈女士' 
+quotes[11]='蒋女士' 
+quotes[12]='沈女士' 
+quotes[13]='韩先生' 
+quotes[14]='杨女士' 
+quotes[15]='朱女士' 
+quotes[16]='秦先生' 
+quotes[17]='许女士' 
+quotes[18]='何女士' 
+quotes[19]='吕女士' 
+quotes[20]='施女士' 
+quotes[21]='张先生' 
+quotes[22]='孔女士'
+quotes[23]='曹先生'
+quotes[24]='严女士'
+quotes[25]='金先生'
+quotes[26]='魏女士'
+quotes[27]='陶女士'
+quotes[28]='姜先生'
+quotes[29]='戚先生'
+quotes[30]='谢先生'
+quotes[31]='邹女士'
+quotes[32]='窦女士'
+quotes[33]='范女士'
+quotes[34]='彭女士'
+quotes[35]='马先生'
+quotes[36]='苗先生'
+quotes[37]='方女士'
+quotes[38]='任先生'
+quotes[39]='袁女士'
+quotes[40]='鲍先生'
+quotes[41]='史女士'
+quotes[42]='唐先生'
+quotes[43]='薛女士'
+quotes[44]='雷先生'
+quotes[45]='倪女士'
+quotes[46]='腾先生'
+quotes[47]='殷女士'
+quotes[48]='罗先生'
+quotes[49]='康先生'
+quotes[50]='余女士'
+quotes[51]='元女士'
+quotes[52]='顾先生'
+quotes[53]='孟先生'
+quotes[54]='黄先生'
+quotes[55]='穆女士'
+	var quote=quotes[rand1]
+	document.write(quote)
+	var rand1=parseInt(Math.random()*5+1)
+	quotes=new Array
+	quotes[1]='（13'+parseInt(Math.random()*10)+ '****'+ parseInt(Math.random()*10)+ parseInt(Math.random()*10)+parseInt(Math.random()*10)+parseInt(Math.random()*10)+'）' 
+	quotes[2]='（15'+parseInt(Math.random()*10)+ '****'+ parseInt(Math.random()*10)+ parseInt(Math.random()*10)+parseInt(Math.random()*10)+parseInt(Math.random()*10)+'）' 
+	quotes[3]='（13'+parseInt(Math.random()*10)+ '****'+ parseInt(Math.random()*10)+ parseInt(Math.random()*10)+parseInt(Math.random()*10)+parseInt(Math.random()*10)+'）' 
+	quotes[4]='（18'+parseInt(Math.random()*10)+ '****'+ parseInt(Math.random()*10)+ parseInt(Math.random()*10)+parseInt(Math.random()*10)+parseInt(Math.random()*10)+'）' 
+	quotes[5]='（13'+parseInt(Math.random()*10)+ '****'+ parseInt(Math.random()*10)+ parseInt(Math.random()*10)+parseInt(Math.random()*10)+parseInt(Math.random()*10)+'）' 
+	var quote=quotes[rand1]
+	document.write(quote)
+	document.write('<br>')
+	var rand1=parseInt(Math.random()*34+1)
+	quotes=new Array
+quotes[1]=' 您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[2]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[3]='  您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[4]='  您订购的 真目明成人装四个疗程完美装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[5]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[6]='  您订购的 真目明青少年装二个疗程加强装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[7]='  您订购的 真目明成人装一个疗程舒缓装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[8]='  您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[9]='  您订购的 真目明成人装三个疗程巩固装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[10]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[11]='  您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[12]='  您订购的 真目明青少年装四个疗程完美装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[13]='  您订购的 真目明成人装三个疗程巩固装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[14]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[15]='  您订购的 真目明成人装四个疗程完美装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[16]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 汇通<font color="#FF0000">√</font>'
+quotes[17]='  您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[18]='  您订购的 真目明成人装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[19]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[20]='  您订购的 真目明成人装二个疗程加强装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[21]='  您订购的 真目明成人装四个疗程完美装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[22]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[23]='  您订购的 真目明成人装二个疗程加强装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[24]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[25]='  您订购的 真目明青少年装二个疗程加强装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[26]='  您订购的 真目明成人装三个疗程巩固装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[27]='  您订购的 真目明成人装一个疗程舒缓装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[28]='  您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[29]='  您订购的 真目明青少年装四个疗程完美装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[30]='  您订购的 真目明成人装三个疗程巩固装 已发货 汇通<font color="#FF0000">√</font>' 
+quotes[31]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 圆通<font color="#FF0000">√</font>' 
+quotes[32]='  您订购的 真目明成人装四个疗程完美装 已发货  圆通<font color="#FF0000">√</font>' 
+quotes[33]='  您订购的 真目明青少年装一个疗程舒缓装 已发货 汇通<font color="#FF0000">√</font>'
+quotes[34]='  您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>'  
+quotes[35]='  您订购的 真目明青少年装二个疗程加强装 已发货 圆通<font color="#FF0000">√</font>'   
+ 
+	var quote=quotes[rand1]
+	document.write(quote)
+	document.write('<br>');
+}
+
+// WFPHPORDER fahuo.js end
---
+++ js/style.js
@@ -0,0 +1,180 @@
+﻿$(document).ready(function(){
+    /**************菜单栏*********************/
+
+    $('.navo>li:first-child>a').css('border-left','none');
+    $('.navo>li:last-child>a').css('border-right','none');
+
+    $('.navo>li').hover(function(){
+        if($(this).children('ul') != ''){
+            $(this).children('ul').css('height','72px');
+            $(this).children('ul').addClass('tran');
+        }
+    },function(){
+        $(this).children('ul').css('height','0');
+    })
+
+    /********************banner************************/
+
+    var baner = $('.baner'),
+        aLi = $('.baner ul').find('li'),
+        aA = $('.squer').find('a'),
+        iNum = 0;
+
+    $('.baner ul li:gt(0)').hide();
+
+    aA.hover(function(){
+        $(this).css('background','#5bc0de').siblings().css('background','');
+        iNum = $(this).index();
+        aLi.eq(iNum).fadeIn('slow').siblings().fadeOut('slow');
+        aLi.stop(true,true);
+    })
+
+    function moveBaner(){
+        if(iNum == aLi.length-1){
+            iNum = -1;
+        }
+        aLi.eq(iNum+1).fadeIn('slow').siblings().fadeOut('slow');
+        aA.eq(iNum+1).css('background','#5bc0de').siblings().css('background','');
+        iNum++;
+    }
+
+    autoplay();
+    function autoplay(){
+        timer = setInterval(function(){
+            moveBaner();
+        },2500)
+    }
+
+    baner.hover(function(){
+        clearInterval(timer);
+    },autoplay);
+
+    /********************boxth************************/
+
+    function textHidden(obj,num){
+        $(obj).each(function(){
+            var maxwidth = num;
+            if($(this).text().length > maxwidth){
+                $(this).text($(this).text().substring(0,maxwidth));
+                $(this).html($(this).html()+'...');
+            }
+        })
+    }
+
+    textHidden($('.boxthT').children('span'),60);
+    textHidden($('.tyxgt ul li').children('a'),21);
+
+    /******************flash视频播放******************/
+
+    var aLi1 = $('.sessC ul').find('li');
+    var aLiw = $('.sessC ul').find('li').width();
+    var oUl1 = $('.sessC').find('ul');
+    var iNum1=0;
+
+        $('.next').click(function() {
+            if (iNum1 < aLi1.length - 3) {
+                iNum1++;
+                oUl1.animate({
+                    'left': -aLiw * iNum1
+                })
+            } else {
+                iNum1 = 0;
+                oUl1.animate({
+                    'left': aLiw * iNum1
+                })
+            }
+        })
+        $('.pre').click(function(){
+            if(iNum1>0){
+                iNum1--;
+                oUl1.animate({
+                    'left' : -aLiw*iNum1
+                })
+            }
+        })
+
+    /*******************回到顶部*************************/
+
+    $('.goTop').children('a').click(function(){
+        $('body,html').animate({
+            'scrollTop' : 0
+        },1500)
+    })
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 400){
+            $('.goTop').css('display','block');
+        }else{
+            $('.goTop').css('display','none');
+        }
+    })
+
+    /********************index sessiont************************/
+
+    function textHiddeno(obj){
+        $(obj).each(function(){
+            var maxlength = 20;
+            if($(this).text().length > maxlength){
+                $(this).text($(this).text().substring(0,maxlength));
+                $(this).html($(this).html()+'...');
+            }
+        })
+    }
+
+    textHiddeno($('.tyxgo p').children('b'));
+    textHiddeno($('.tyxgo p').children('span'));
+
+    /********************在线客服************************/
+
+    $('.zxkf').click(function(){
+        $('.olLeft').show(500);
+        $(this).css('display','none');
+        $('.gbkf').css('display','block');
+    })
+    $('.gbkf').click(function(){
+        $('.olLeft').hide(500);
+        $('.zxkf').css('display','block');
+        $(this).css('display','none');
+    })
+
+    /****************真实案例****************/
+
+    var aLi2 = $('.anlio').children('li'),
+        aLi3 = $('.anlit').children('li');
+
+    $('.anlio li>p').click(function(){
+        var aLi3Index = aLi2.index($(this).parent());
+
+        $('.anlit').css('display','block')
+        aLi3.eq(aLi3Index).show().siblings().hide();
+        $('.anlio').css('display','none');
+    })
+
+    $('.anlit li>a').click(function(){
+
+        $(this).parent().hide();
+        $('.anlio').css('display','block');
+    })
+
+    /*******************图片滚动**********************/
+
+    var oUlo = $('.qwmto').children('ul')[0];
+
+    oUlo.innerHTML += oUlo.innerHTML;
+
+    function hd(){
+        if(oUlo.offsetTop < -oUlo.offsetHeight/2){
+            oUlo.style.top = '0';
+        }
+        oUlo.style.top = oUlo.offsetTop - 1+'px';
+    }
+
+    var timer1 = setInterval(hd,30);
+
+    $('.qwmt').hover(function(){
+        clearInterval(timer1);
+    },function(){
+        timer1 = setInterval(hd,30);
+    })
+
+})
